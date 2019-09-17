dadfd655049fb9d528291b3b4044976476426df6
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2019-09-17 15:00:57 +0900

    add window_under(), window_under_zero() and window_under_zero_value()

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,15 @@ pub trait Trta<T> {
 
     fn window(&self, period: usize) -> Vec<Vec<T>>;
     // (1 2 3 4 5) => ((1 2 3) (2 3 4) (3 4 5))
+
+    fn window_under_zero(&self, period: usize) -> Vec<Vec<T>>;
+    // (1 2 3 4 5) => ((0) (0) (1 2 3) (2 3 4) (3 4 5))
+
+    fn window_under(&self, period: usize) -> Vec<Vec<T>>;
+    // (1 2 3 4 5) => ((1) (1 2) (1 2 3) (2 3 4) (3 4 5))
+
+    fn window_under_zero_value(&self, period: usize) -> Vec<Vec<T>>;
+    // (1 2 3 4 5) => ((0 0 1) (0 1 2) (1 2 3) (2 3 4) (3 4 5))
 }
 
 impl Trta<i32> for [i32] {
@@ -24,6 +33,37 @@ impl Trta<i32> for [i32] {
 
         v
     }
+
+    fn window_under_zero(&self, period: usize) -> Vec<Vec<i32>> {
+        let mut v = vec![vec![0]; period - 1];
+
+        v.extend(self.window(period));
+
+        v
+    }
+
+    fn window_under(&self, period: usize) -> Vec<Vec<i32>> {
+        let mut v: Vec<Vec<i32>> = Vec::new();
+
+        for i in 1..period {
+            v.push(self.to_vec().into_iter().take(i).collect::<Vec<_>>());
+        }
+        // create shorter vectors than period
+
+        for i in 0..=(self.len() - period) {
+            v.push(self.to_vec().into_iter().skip(i).take(period).collect::<Vec<_>>());
+        }
+
+        v
+    }
+
+    fn window_under_zero_value(&self, period: usize) -> Vec<Vec<i32>> {
+        let mut v = vec![0; period - 1];
+
+        v.extend(self);
+
+        v.window(period)
+    }
 }
 
 impl Trta<f64> for [f64] {
@@ -43,6 +83,37 @@ impl Trta<f64> for [f64] {
         }
 
         v
+    }
+
+    fn window_under_zero(&self, period: usize) -> Vec<Vec<f64>> {
+        let mut v = vec![vec![0.0]; period - 1];
+
+        v.extend(self.window(period));
+
+        v
+    }
+
+    fn window_under(&self, period: usize) -> Vec<Vec<f64>> {
+        let mut v: Vec<Vec<f64>> = Vec::new();
+
+        for i in 1..period {
+            v.push(self.to_vec().into_iter().take(i).collect::<Vec<_>>());
+        }
+        // create shorter vectors than period
+
+        for i in 0..=(self.len() - period) {
+            v.push(self.to_vec().into_iter().skip(i).take(period).collect::<Vec<_>>());
+        }
+
+        v
+    }
+
+    fn window_under_zero_value(&self, period: usize) -> Vec<Vec<f64>> {
+        let mut v = vec![0.0; period - 1];
+
+        v.extend(self);
+
+        v.window(period)
     }
 }
 
@@ -84,5 +155,65 @@ mod tests {
                 [[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [3.0, 4.0, 5.0]]);
         assert_eq!(vec![1.0, 2.0, 3.0, 4.0, 5.0].window(3),
                 [[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [3.0, 4.0, 5.0]]);
+    }
+
+    #[test]
+    fn test_window_under_zero() {
+        assert_eq!([1, 2, 3, 4, 5].window_under_zero(3),
+                [vec![0], vec![0], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+        assert_eq!((&[1, 2, 3, 4, 5]).window_under_zero(3),
+                [vec![0], vec![0], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+        assert_eq!(vec![1, 2, 3, 4, 5].window_under_zero(3),
+                [vec![0], vec![0], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+
+        assert_eq!([1.0, 2.0, 3.0, 4.0, 5.0].window_under_zero(3),
+                [vec![0.0], vec![0.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+        assert_eq!((&[1.0, 2.0, 3.0, 4.0, 5.0]).window_under_zero(3),
+                [vec![0.0], vec![0.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+        assert_eq!(vec![1.0, 2.0, 3.0, 4.0, 5.0].window_under_zero(3),
+                [vec![0.0], vec![0.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+    }
+
+    #[test]
+    fn test_window_under() {
+        assert_eq!([1, 2, 3, 4, 5].window_under(3),
+                [vec![1], vec![1, 2], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+        assert_eq!((&[1, 2, 3, 4, 5]).window_under(3),
+                [vec![1], vec![1, 2], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+        assert_eq!(vec![1, 2, 3, 4, 5].window_under(3),
+                [vec![1], vec![1, 2], vec![1, 2, 3], vec![2, 3, 4], vec![3, 4, 5]]);
+
+        assert_eq!([1.0, 2.0, 3.0, 4.0, 5.0].window_under(3),
+                [vec![1.0], vec![1.0, 2.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+        assert_eq!((&[1.0, 2.0, 3.0, 4.0, 5.0]).window_under(3),
+                [vec![1.0], vec![1.0, 2.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+        assert_eq!(vec![1.0, 2.0, 3.0, 4.0, 5.0].window_under(3),
+                [vec![1.0], vec![1.0, 2.0],
+                vec![1.0, 2.0, 3.0], vec![2.0, 3.0, 4.0], vec![3.0, 4.0, 5.0]]);
+    }
+
+    #[test]
+    fn test_window_under_zero_value() {
+        assert_eq!([1, 2, 3, 4, 5].window_under_zero_value(3),
+                [[0, 0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]]);
+        assert_eq!((&[1, 2, 3, 4, 5]).window_under_zero_value(3),
+                [[0, 0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]]);
+        assert_eq!(vec![1, 2, 3, 4, 5].window_under_zero_value(3),
+                [[0, 0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]]);
+
+        assert_eq!([1.0, 2.0, 3.0, 4.0, 5.0].window_under_zero_value(3),
+                [[0.0, 0.0, 1.0], [0.0, 1.0, 2.0],
+                [1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [3.0, 4.0, 5.0]]);
+        assert_eq!((&[1.0, 2.0, 3.0, 4.0, 5.0]).window_under_zero_value(3),
+                [[0.0, 0.0, 1.0], [0.0, 1.0, 2.0],
+                [1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [3.0, 4.0, 5.0]]);
+        assert_eq!(vec![1.0, 2.0, 3.0, 4.0, 5.0].window_under_zero_value(3),
+                [[0.0, 0.0, 1.0], [0.0, 1.0, 2.0],
+                [1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [3.0, 4.0, 5.0]]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use trta::Trta;
 
 fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let window = v.window_under_zero_value(3);
+
+    println!("{:?}", window);  //=> [[0, 0, 1], [0, 1, 2], [1, 2, 3], [2, 3, 4], [3, 4, 5]]
 }
